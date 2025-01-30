59e80bffe9355ca3513bd3fb0a18c2c456fc9c15
Susmith A J <susmith508@gmail.com>
2025-01-30 20:24:21 +0530

    feat/sidebar: sidebar implementation

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,67 +1,131 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+"use client"
+
+import * as React from "react"
+
+import { BookOpen, Calculator, ChevronsUpDown, EarthLock, Flag, GalleryVerticalEnd, Settings2, } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
+  SidebarFooter,
+  SidebarHeader,
   SidebarMenuButton,
-  SidebarMenuItem,
+  SidebarRail,
 } from "@/components/ui/sidebar"
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
+import { NavMain } from "@/components/ui/nav-main"
 
-export function AppSidebar() {
+// Menu items.
+const data = {
+  toolsown: {
+    name: "ToolsOwn",
+    logo: GalleryVerticalEnd,
+    subTitle: "Simplified Finance Calculator Hub",
+  },
+  navMain: [
+    {
+      title: "Finance Calculators",
+      url: "#",
+      icon: Calculator,
+      isActive: true,
+      items: [
+        {
+          title: "Income Tax Calculator",
+          url: "#",
+        },
+        {
+          title: "EMI Calculator",
+          url: "#",
+        },
+        {
+          title: "Mutual Fund Calculatore",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Crypto Finance Calculators",
+      url: "#",
+      icon: EarthLock,
+      items: [
+        {
+          title: "Profit Calculator",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Blogs",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Finance",
+          url: "#",
+        },
+        {
+          title: "Crypto",
+          url: "#",
+        },
+        {
+          title: "Wealth",
+          url: "#",
+        },
+        {
+          title: "Fin-tech",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Report",
+          url: "#",
+        },
+        {
+          title: "About Us",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-semibold">
+              {data.toolsown.name}
+            </span>
+            <span className="truncate text-xs">{data.toolsown.subTitle}</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarHeader>
+      <SidebarMenuButton asChild>
+  <a href="#">Home</a>
+</SidebarMenuButton>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavMain items={data.navMain} />
       </SidebarContent>
+      <SidebarFooter />
+      <SidebarRail />
     </Sidebar>
   )
 }
